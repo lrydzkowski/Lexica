104aad508db4096826843d35f618ee5f0c5d1733
Łukasz Rydzkowski <lukasz_rydzkowski@loco.pl>
2020-09-24 21:39:07 +0200

    Start writing core library tests.

---
+++ CoreTests/AppSettingsTests.cs
@@ -0,0 +1,24 @@
+﻿using Lexica.Core.Config;
+using Lexica.Core.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CoreTests
+{
+    public class AppSettingsTests
+    {
+        [Theory]
+        [InlineData("Resources.appsettings1.json")]
+        [InlineData("Resources.appsettings2.json")]
+        public void Load_WrongConfiguration_ThrowsWrongConfigException(string resourcePath)
+        {
+            var resourceConfigSource = new ResourceConfigSource(resourcePath);
+            //AppSettings appSettings = new AppSettings(resourceConfigSource);
+
+            WrongConfigException ex = Assert.Throws<WrongConfigException>(() => new AppSettings(resourceConfigSource));
+            Assert.Equal("App settings has a wrong structure.", ex.Message);
+        }
+    }
+}
---
+++ CoreTests/ResourceConfigSource.cs
@@ -0,0 +1,30 @@
+﻿using Lexica.Core.Config;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace CoreTests
+{
+    class ResourceConfigSource : IConfigSource
+    {
+        public string Path { get; private set; }
+
+        public ResourceConfigSource(string path)
+        {
+            Path = path;
+        }
+
+        public string GetContents()
+        {
+            var resourceLoader = new ResourceLoader();
+            string contents = resourceLoader.GetEmbeddedResourceString(Assembly.GetExecutingAssembly(), Path);
+            if (contents == null)
+            {
+                throw new Exception(String.Format("Resource {0} doesn't exist.", Path));
+            }
+
+            return contents;
+        }
+    }
+}
