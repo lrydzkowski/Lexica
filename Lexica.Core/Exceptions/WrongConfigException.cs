6665c69fa6f1f1c7ae7fed82511efaada93de406
Łukasz Rydzkowski <lukasz_rydzkowski@loco.pl>
2020-10-17 19:24:39 +0200

    Change custom exceptions in Core library

--- Lexica.Core/Exceptions/WrongConfigException.cs
+++ Lexica.Core/Exceptions/WrongConfigException.cs
@@ -4,12 +4,17 @@ using System.Text;
 
 namespace Lexica.Core.Exceptions
 {
-    public class WrongConfigException : AppException
+    [Serializable]
+    public class WrongConfigException : ApplicationException
     {
         public WrongConfigException() { }
 
         public WrongConfigException(string message) : base(message) { }
 
         public WrongConfigException(string message, Exception inner) : base(message, inner) { }
+
+        protected WrongConfigException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
 }
