8ad7a5460a5a238b3f5a5add5b9955b738408017
Łukasz Rydzkowski <lukasz_rydzkowski@loco.pl>
2020-10-27 19:59:29 +0100

    Add namespace editing operation to EF module

---
+++ Lexica.EF/Services/NamespaceService.cs
@@ -0,0 +1,31 @@
+﻿using Lexica.Core.Models;
+using Lexica.EF.Models;
+using Lexica.Words.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexica.EF.Services
+{
+    class NamespaceService : INamespaceService
+    {
+        public async Task<OperationResult> ChangePath(string oldNamespacePath, string newNamespacePath)
+        {
+            using (var context = new LexicaContext())
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                List<SetTable> sets = await context.Sets.Where(x => x.Namespace == oldNamespacePath).ToListAsync();
+                foreach (SetTable set in sets)
+                {
+                    set.Namespace = newNamespacePath;
+                }
+                await context.SaveChangesAsync();
+                await transaction.CommitAsync();
+            }
+            return new OperationResult(true);
+        }
+    }
+}
