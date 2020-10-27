a9381b27e666171e190a7ece4a880ea1061b5a4d
Łukasz Rydzkowski <lukasz_rydzkowski@loco.pl>
2020-10-27 20:12:36 +0100

    Add config to Lexica.Words library

---
+++ Lexica.Words/Config/Models/Words.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lexica.Words.Config.Models
+{
+    class Words
+    {
+        public string? ImportDirectoryPath { get; set; }
+    }
+}
