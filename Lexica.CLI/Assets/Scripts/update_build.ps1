d97752599753a1e57e8951b02058395238f95e4a
Łukasz Rydzkowski <lukasz_rydzkowski@loco.pl>
2021-02-23 21:07:41 +0100

    CLI: Build mechanisms

---
+++ Lexica.CLI/Assets/Scripts/update_build.ps1
@@ -0,0 +1,18 @@
+﻿#
+# Script responsible for updating a build number which is stored in build file in the main directory.
+#
+# Script requires PowerShell 7 (https://github.com/PowerShell/PowerShell)
+#
+
+try
+{
+    $build = (Get-Date -format "yyyy-MM-ddTHH:mm")
+    $build | Set-Content -NoNewline -Path "$PSScriptRoot\..\..\build"
+}
+catch
+{
+	Write-Host "An error occured:" -ForegroundColor red;
+    Write-Host $_ -ForegroundColor red;
+    Write-Host $_.ErrorDetails -ForegroundColor red;
+    Write-Host $_.ScriptStackTrace -ForegroundColor red;
+}
