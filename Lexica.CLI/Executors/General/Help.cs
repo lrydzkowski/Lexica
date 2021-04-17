a61a5e45d96d2871bb92507ae034409661625ea8
Łukasz Rydzkowski <lukasz_rydzkowski@loco.pl>
2021-04-17 14:59:03 +0200

    Implement small corrections in help presentation

--- Lexica.CLI/Executors/General/Help.cs
+++ Lexica.CLI/Executors/General/Help.cs
@@ -8,7 +8,7 @@ namespace Lexica.CLI.Executors.General
         public void Execute(List<string>? args = null)
         {
             Console.WriteLine("Info:    English learning application for Windows.");
-            Console.WriteLine("Usage:   Lexica.CLI.exe [COMMANDS]");
+            Console.WriteLine("Usage:   Lexica.CLI.exe [command]");
             Console.WriteLine();
             Console.WriteLine("Commands:");
             Console.WriteLine("  -h  | --help      Show help.");
--- Lexica.CLI/Executors/Modes/ModeHelp.cs
+++ Lexica.CLI/Executors/Modes/ModeHelp.cs
@@ -8,12 +8,12 @@ namespace Lexica.CLI.Executors.Modes
         public void Execute(List<string>? args = null)
         {
             Console.WriteLine("Info:    Run learning mode.");
-            Console.WriteLine("Usage:   Lexica run [COMMANDS]");
+            Console.WriteLine("Usage:   Lexica run --mode [mode]");
             Console.WriteLine();
             Console.WriteLine("Commands:");
             Console.WriteLine("  -m  | --mode          Run mode.");
             Console.WriteLine();
-            Console.WriteLine("Available modes:");
+            Console.WriteLine("Modes:");
             Console.WriteLine("  s   | spelling        Check your spelling.");
             Console.WriteLine("  f   | full            Full path of learning with closed and open questions.");
             Console.WriteLine("  oo  | only-open       Only open questions.");
