f32b75c79cc7a9ddb88055a36ec072c0c38c5841
Łukasz Rydzkowski <lukasz_rydzkowski@loco.pl>
2021-04-19 21:42:26 +0200

    Init Lexica.CLI configuration script

---
+++ Lexica.CLI/Assets/Scripts/init_appsettings.ps1
@@ -0,0 +1,21 @@
+﻿#
+# Script responsible for configuration file initialization (appsettings.json).
+#
+
+try
+{
+    $sampleAppSettingsPath = "$PSScriptRoot\..\..\appsettings.sample.json"
+    $appSettingsPath = "$PSScriptRoot\..\..\appsettings.json"
+    if (!(Test-Path $appSettingsPath))
+    {
+        Copy-Item -Path $sampleAppSettingsPath -Destination $appSettingsPath
+    }
+}
+catch
+{
+	Write-Host "An error occured:" -ForegroundColor red;
+    Write-Host $_ -ForegroundColor red;
+    Write-Host $_.ErrorDetails -ForegroundColor red;
+    Write-Host $_.ScriptStackTrace -ForegroundColor red;
+    exit 1
+}
